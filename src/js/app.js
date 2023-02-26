b698ad6f0d66f2e9786e72313b0bcf97b78af34a
Katerina Zhuravlevich <95651554+baekatinee@users.noreply.github.com>
2023-02-26 18:20:34 +0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,30 @@ flsFunctions.isWebp();
 import Swiper, {Navigation, Pagination} from 'swiper';
 
 const swiper = new Swiper();  
+$('.header_inner-menu-burger-menu').on('click', function() {
+    $('.header_inner-menu-list').slideToggle('active'); 
+  });
+  $('.question_inner-question').on('click', function() {
+    $(this).next('.question_inner-question-box').slideToggle('active');
+    $(this).children('.question_inner-question-info-arrow').toggleClass('active');
+  });
+
+  $('.question_inner-question').on('click', function(){
+    $(this)
+      .closest('.question_inner-question')
+      .find('.question_inner-question-box')
+      .slideToggle('active');
+  });
+  $('.question_inner-question').on('click', function(){
+    $(this)
+      .closest('.question_inner-question')
+      .find('.question_inner-question-info-arrow')
+      .toggleClass('active');
+  });
+
+  const slider = new A11YSlider(document.querySelector('.history_inner'), {
+    slidesToShow: 1,
+    arrows: true, // arrows enabled 767px and down
+    dots: false,
+   
+  });
