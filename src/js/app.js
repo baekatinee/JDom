05cbce706ea1631d47d9b28325a689f956a3fa28
Katerina Zhuravlevich <95651554+baekatinee@users.noreply.github.com>
2023-03-11 02:04:21 +0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -2,35 +2,41 @@ import *as flsFunctions from "./modules/functions.js"
 
 flsFunctions.isWebp();
 
-import Swiper, {Navigation, Pagination} from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper();  
-$('.header_inner-menu-burger-menu').on('click', function() {
-    $('.header_inner-menu-list').slideToggle('active'); 
-  });
-  $('.question_inner-question').on('click', function() {
-    $(this).next('.question_inner-question-box').slideToggle('active');
-    $(this).children('.question_inner-question-info-arrow').toggleClass('active');
-  });
+const swiper = new Swiper();
+$('.header_inner-menu-burger-menu').on('click', function () {
+  $('.header_inner-menu-list').slideToggle('active');
+});
+$('.question_inner-question').on('click', function () {
+  $(this).next('.question_inner-question-box').slideToggle('active');
+  $(this).children('.question_inner-question-info-arrow').toggleClass('active');
+});
 
-  $('.question_inner-question').on('click', function(){
-    $(this)
-      .closest('.question_inner-question')
-      .find('.question_inner-question-box')
-      .slideToggle('active');
-  });
-  $('.question_inner-question').on('click', function(){
-    $(this)
-      .closest('.question_inner-question')
-      .find('.question_inner-question-info-arrow')
-      .toggleClass('active');
-  });
+$('.question_inner-question').on('click', function () {
+  $(this)
+    .closest('.question_inner-question')
+    .find('.question_inner-question-box')
+    .slideToggle('active');
+});
+$('.question_inner-question').on('click', function () {
+  $(this)
+    .closest('.question_inner-question')
+    .find('.question_inner-question-info-arrow')
+    .toggleClass('active');
+});
 
-  const slider = new A11YSlider(document.querySelector('.history_inner'), {
-    slidesToShow: 1,
-    arrows: true, // arrows enabled 767px and down
-    dots: false,
-    customPaging: function(index, a11ySlider) {
-      return '<button class="mycustombtn">' + index + '</button>';
+const slider = new A11YSlider(document.querySelector('.history_inner'), {
+  slidesToShow: 1,
+  arrows: true, // arrows enabled 767px and down
+  dots: false,
+  responsive: {
+
+    320: {
+      disable: true // slider disabled 960px to 1279px
+    },
+    651: {
+      disable: false,
+    }
   }
-  });
+});
